e0666f6e2dd7b254144726498115aaeafe705726
alexis <alxwu825@yahoo.com>
2024-10-17 09:55:40 +0200

    sync with hp

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
-require("dotenv").config()
-require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
 
-const privateKey = process.env.PRIVATE_KEY || ""
+const privateKey = process.env.PRIVATE_KEY || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -10,7 +10,11 @@ module.exports = {
     hardhat: {
       forking: {
         url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      }
-    }
-  }
+      },
+    },
+    mainnet: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [privateKey],
+    },
+  },
 };
